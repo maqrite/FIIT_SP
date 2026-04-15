26d5a7aaed68957f619b396a3a125cbf47ba0c8e
Grikhanov Artem <artemgrihanov@ya.ru>
2026-04-16 00:25:22 +0300

    sm fixes and preps5

--- associative_container/indexing_tree/b_tree/src/main.cpp
+++ associative_container/indexing_tree/b_tree/src/main.cpp
@@ -2,3 +2,28 @@
 #include "pp_allocator.h"
 #include <iostream>
 #include <string>
+
+int main() {
+  try {
+    test_mem_resource mr;
+    pp_allocator<std::pair<const int, std::string>> alloc(&mr);
+
+    B_tree<int, std::string, std::less<int>, 3> tree(alloc);
+
+    std::cout << "Дерево успешно создано." << std::endl;
+
+    int key = 10;
+    if (tree.contains(key)) {
+      std::cout << "Критическая ошибка: ключ найден в пустом дереве!"
+                << std::endl;
+    } else {
+      std::cout << "Поиск в пустом дереве работает корректно (ключ не найден)."
+                << std::endl;
+    }
+
+  } catch (const std::exception &e) {
+    std::cerr << "Поймали исключение: " << e.what() << std::endl;
+  }
+
+  return 0;
+}
